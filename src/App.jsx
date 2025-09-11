ed29ee730db0f488d7e6ed817945cfa65a98193a
Dewmini-Kaveesha <dewminikaveeshaa@gmail.com>
2025-09-12 02:49:36 +0530

    Add updated navbar component

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,13 @@
 
 import './App.css'
+import Navbar from './components/Navbar'
 
 function App() {
   
 
   return (
     <>
-      <h1 className='text-4xl font-bold'>Hello, Developers!</h1>
+      <Navbar/>
     </>
   )
 }
