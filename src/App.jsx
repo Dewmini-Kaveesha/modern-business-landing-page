b63267fb8740523684fce2dfa744d761ca11f99b
Dewmini-Kaveesha <dewminikaveeshaa@gmail.com>
2025-09-12 03:26:49 +0530

    Resolved merge conflict in App.js and added Navbar + Hero

--- src/App.jsx
+++ src/App.jsx
@@ -7,11 +7,11 @@ function App() {
   
 
   return (
-<<<<<<< HEAD
+
     <>
       <Navbar/>
-    </>
-=======
+    
+
     
       <main className="relative min-h-screen overflow-x-hidden">
         <div className="absolute -top-28 -left-28 w-[500px] h-[500px] bg-gradient-to-tr from-indigo-500/20 to-pink-500/20 rounded-full blur-[80px] -z-10"></div>
@@ -21,7 +21,7 @@ function App() {
       </main>
 
     
->>>>>>> ffafa7b0d6df652eb4ae0c04208bf35ca9d051ac
+</>
   )
 }
 
