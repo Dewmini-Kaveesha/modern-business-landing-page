f7222a5e7b7c28138cbe7acfe541dc5ac3767a40
Sachithmadushanka <sachithmadushanka0104@gmail.com>
2025-09-27 21:53:06 +0530

    created ScheduleSection

---
+++ src/components/ScheduleSection.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const ScheduleSection = () => {
+  return (
+    <section className='max-w-7xl mx-auto px-4 py-16 md:py-24'>
+      <div className='flex flex-col md:flex-row items-center justify-between gap-12 md:gap-24'>
+        {/* left section */}
+        <div > left</div>
+        {/* right section */}
+        <div>right</div>
+      </div>
+    </section>
+  )
+}
+
+export default ScheduleSection
