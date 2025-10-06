2fbb9021eeb755233cf2b6b57e22cfe726a71e06
Sachithmadushanka <sachithmadushanka0104@gmail.com>
2025-10-06 23:53:41 +0530

    Created the FooterLinks in the footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,31 @@
 import React from 'react'
+import { BiCategory } from 'react-icons/bi'
 import { FaTwitter } from 'react-icons/fa'
 import { FaFacebookF, FaLinkedin } from 'react-icons/fa6'
+const footerLinks = {
+    company: [
+      { name: 'About', href: '#' },
+      { name: 'Terms of Use', href: '#' },
+      { name: 'Privacy Policy', href: '#' },
+      { name: 'How it Works', href: '#' },
+      { name: 'Contact Us', href: '#' },
+    ],
+    getHelp: [
+      { name: 'Support Carrer', href: '#' },
+      { name: '24h Service', href: '#' },
+      { name: 'Quick Chat', href: '#' },
+    ],
+    support: [
+      { name: 'FAQ', href: '#' },
+      { name: 'Policy', href: '#' },
+      { name: 'Business', href: '#' },
+    ],
+    contact: [
+      { name: 'WhatsApp', href: '#' },
+      { name: 'Support 24', href: '#' },
+    ],
+  }
+
 
 const Footer = () => {
   return (
@@ -31,6 +56,15 @@ const Footer = () => {
                     className='size-5'/></a>
                     </div>
                 </div>
+
+                {/*footer nav items*/}
+                <div>
+                    <div>
+                       
+                    </div>
+                </div>
+
+
             </div>
         </div>
     </footer>
